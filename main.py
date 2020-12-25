9fb3a9a5e468ed496f55efb784a5b0fcb8d16dbc
Znunu <61394004+znunu@users.noreply.github.com>
2020-12-25 19:48:58 +0100

    commit

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import settings
 import cog
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=settings.prefix)  # YOUR PREFIX HERE
+bot = commands.Bot(command_prefix=settings.prefix)  # YOUR PREFIX HERE e.g. bot = commands.Bot(command_prefix="! ")
 bot.add_cog(cog.Mudae(bot))
-bot.run(settings.token)  # YOUR TOKEN HERE
+bot.run(settings.token)  # YOUR TOKEN HERE e.g. bot.run("ABCDEFSDS3434334")
