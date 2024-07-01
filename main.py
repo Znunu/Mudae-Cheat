3f0b5a7f1d79330f4108d201c363cd26d12c562d
Znunu <61394004+Znunu@users.noreply.github.com>
2024-07-01 13:33:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import cog
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="YOUR PREFIX HERE")
+bot = commands.Bot(command_prefix="YOUR PREFIX HERE", intents=discord.Intents.all())
 
 bot.add_cog(cog.Mudae(bot))
 
 # UNCOMMENT THIS FOR BOT
-# bot.run("YOUR TOKEN HERE", intents=discord.Intents.all())
+# bot.run("YOUR TOKEN HERE")
 
 # UNCOMMENT THIS FOR USER BOT
-# bot.login("YOUR TOKEN HERE", bot=false, intents=discord.Intents.all())
+# bot.login("YOUR TOKEN HERE", bot=false)
